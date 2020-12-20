28d35a68d818358d0cc73baa477208768446f86d
alexchuprin <btnevermind@gmail.com>
2020-12-20 12:42:00 +0200

    added actual flatten solution

--- flatten.exs
+++ flatten.exs
@@ -1,20 +1,14 @@
-defmodule ArrayLeaders do
-  def find_leader(number, []) when number > 0, do: [number]
-  def find_leader(number, []) when number <= 0, do: []
-  def find_leader(number, leaders) do
-    if(number > Enum.sum(leaders), do: (leaders ++ [number]),
-    else: leaders)
+defmodule Flatten do
+  def unwrap(ele, acc) when (is_list ele) do
+    flat(ele, acc)
   end
-  def get_sum([]), do: 0
-  def get_sum(list) do
-    Enum.sum(list)
+  def unwrap(ele, acc) do
+    acc ++ [ele]
   end
-  def is_leader({num, index}, list), do: num > get_sum(List.delete_at(list, index))
-  def array_leaders(nums) do
-    nums
-    |> Enum.with_index
-    |> Enum.filter(&(is_leader &1, nums))
-    |> Enum.map(fn {a,i}  -> a end)
 
+  def flat(list), do: flat(list, [])
+  def flat(list, res) do
+    list
+    |> List.foldl(res, &(unwrap/2))
   end
 end
